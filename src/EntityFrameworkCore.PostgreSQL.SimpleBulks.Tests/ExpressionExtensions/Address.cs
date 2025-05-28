37493a6c37c9f3c418113b2d229ab74e1a96dae3
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-05-28 23:31:10 +0700

    Support Nested Properties

---
+++ src/EntityFrameworkCore.PostgreSQL.SimpleBulks.Tests/ExpressionExtensions/Address.cs
@@ -0,0 +1,14 @@
+﻿namespace EntityFrameworkCore.PostgreSQL.SimpleBulks.Tests.ExpressionExtensions;
+
+public class Address
+{
+    public string Street { get; set; }
+
+    public string City { get; set; }
+
+    public string State { get; set; }
+
+    public string ZipCode { get; set; }
+
+    public string Country { get; set; }
+}
