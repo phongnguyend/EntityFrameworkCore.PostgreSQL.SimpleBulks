186036962167acab621091d8b27e5ba81a450313
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-05-30 09:03:52 +0700

    Cache TableInfo

---
+++ src/EntityFrameworkCore.PostgreSQL.SimpleBulks.Tests/DbContextExtensions/GetTableInforTests.cs
@@ -0,0 +1,35 @@
+﻿using EntityFrameworkCore.PostgreSQL.SimpleBulks.Extensions;
+using EntityFrameworkCore.PostgreSQL.SimpleBulks.Tests.Database;
+
+namespace EntityFrameworkCore.PostgreSQL.SimpleBulks.Tests.DbContextExtensions;
+
+public class GetTableInforTests
+{
+    [Fact]
+    public void GetTableInfor_ReturnsCorrectTableInformation()
+    {
+        // Arrange
+        var dbContext = new TestDbContext("", "");
+
+        // Act
+        var tableInfor = dbContext.GetTableInfor(typeof(ConfigurationEntry));
+
+        // Assert
+        Assert.Equal("ConfigurationEntry", tableInfor.Name);
+        Assert.Equal("\"ConfigurationEntry\"", tableInfor.SchemaQualifiedTableName);
+    }
+
+    [Fact]
+    public void GetTableInfor_ReturnsFromCache()
+    {
+        // Arrange
+        var dbContext = new TestDbContext("", "");
+
+        // Act
+        var tableInfor1 = dbContext.GetTableInfor(typeof(ConfigurationEntry));
+        var tableInfor2 = dbContext.GetTableInfor(typeof(ConfigurationEntry));
+
+        // Assert
+        Assert.Equal(tableInfor1, tableInfor2);
+    }
+}
