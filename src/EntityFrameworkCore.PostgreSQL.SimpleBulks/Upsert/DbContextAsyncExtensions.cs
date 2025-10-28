01a4bff15e81b288d186415e1174f44d0748911f
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-10-28 10:39:26 +0700

    Upsert

---
+++ src/EntityFrameworkCore.PostgreSQL.SimpleBulks/Upsert/DbContextAsyncExtensions.cs
@@ -0,0 +1,70 @@
+﻿using EntityFrameworkCore.PostgreSQL.SimpleBulks.BulkMerge;
+using EntityFrameworkCore.PostgreSQL.SimpleBulks.Extensions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.PostgreSQL.SimpleBulks.Upsert;
+
+public static class DbContextAsyncExtensions
+{
+    public static Task<BulkMergeResult> UpsertAsync<T>(this DbContext dbContext, T data, Expression<Func<T, object>> idSelector, Expression<Func<T, object>> updateColumnNamesSelector, Expression<Func<T, object>> insertColumnNamesSelector, Action<BulkMergeOptions> configureOptions = null, CancellationToken cancellationToken = default)
+    {
+        var connection = dbContext.GetNpgsqlConnection();
+        var transaction = dbContext.GetCurrentNpgsqlTransaction();
+        var outputIdColumn = dbContext.GetOutputId(typeof(T))?.PropertyName;
+
+        return new BulkMergeBuilder<T>(connection, transaction)
+             .WithId(idSelector)
+             .WithUpdateColumns(updateColumnNamesSelector)
+             .WithInsertColumns(insertColumnNamesSelector)
+             .WithDbColumnMappings(dbContext.GetColumnNames(typeof(T)))
+             .WithDbColumnTypeMappings(dbContext.GetColumnTypes(typeof(T)))
+             .WithValueConverters(dbContext.GetValueConverters(typeof(T)))
+             .WithOutputId(outputIdColumn)
+             .ToTable(dbContext.GetTableInfor(typeof(T)))
+             .ConfigureBulkOptions(configureOptions)
+             .SingleMergeAsync(data, cancellationToken);
+    }
+
+    public static Task<BulkMergeResult> UpsertAsync<T>(this DbContext dbContext, T data, string idColumn, IEnumerable<string> updateColumnNames, IEnumerable<string> insertColumnNames, Action<BulkMergeOptions> configureOptions = null, CancellationToken cancellationToken = default)
+    {
+        var connection = dbContext.GetNpgsqlConnection();
+        var transaction = dbContext.GetCurrentNpgsqlTransaction();
+        var outputIdColumn = dbContext.GetOutputId(typeof(T))?.PropertyName;
+
+        return new BulkMergeBuilder<T>(connection, transaction)
+            .WithId(idColumn)
+            .WithUpdateColumns(updateColumnNames)
+            .WithInsertColumns(insertColumnNames)
+            .WithDbColumnMappings(dbContext.GetColumnNames(typeof(T)))
+            .WithDbColumnTypeMappings(dbContext.GetColumnTypes(typeof(T)))
+            .WithValueConverters(dbContext.GetValueConverters(typeof(T)))
+            .WithOutputId(outputIdColumn)
+            .ToTable(dbContext.GetTableInfor(typeof(T)))
+            .ConfigureBulkOptions(configureOptions)
+            .SingleMergeAsync(data, cancellationToken);
+    }
+
+    public static Task<BulkMergeResult> UpsertAsync<T>(this DbContext dbContext, T data, IEnumerable<string> idColumns, IEnumerable<string> updateColumnNames, IEnumerable<string> insertColumnNames, Action<BulkMergeOptions> configureOptions = null, CancellationToken cancellationToken = default)
+    {
+        var connection = dbContext.GetNpgsqlConnection();
+        var transaction = dbContext.GetCurrentNpgsqlTransaction();
+        var outputIdColumn = dbContext.GetOutputId(typeof(T))?.PropertyName;
+
+        return new BulkMergeBuilder<T>(connection, transaction)
+            .WithId(idColumns)
+            .WithUpdateColumns(updateColumnNames)
+            .WithInsertColumns(insertColumnNames)
+            .WithDbColumnMappings(dbContext.GetColumnNames(typeof(T)))
+            .WithDbColumnTypeMappings(dbContext.GetColumnTypes(typeof(T)))
+            .WithValueConverters(dbContext.GetValueConverters(typeof(T)))
+            .WithOutputId(outputIdColumn)
+            .ToTable(dbContext.GetTableInfor(typeof(T)))
+            .ConfigureBulkOptions(configureOptions)
+            .SingleMergeAsync(data, cancellationToken);
+    }
+}
