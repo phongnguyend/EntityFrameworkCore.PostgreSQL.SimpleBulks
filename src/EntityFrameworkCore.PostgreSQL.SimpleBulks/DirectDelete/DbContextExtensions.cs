049a2095091394bf65ca02075aaddbfe5d980c9c
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-12-20 21:14:36 +0700

    BulkDelete

--- src/EntityFrameworkCore.PostgreSQL.SimpleBulks/DirectDelete/DbContextExtensions.cs
+++ src/EntityFrameworkCore.PostgreSQL.SimpleBulks/DirectDelete/DbContextExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using EntityFrameworkCore.PostgreSQL.SimpleBulks.BulkDelete;
 using EntityFrameworkCore.PostgreSQL.SimpleBulks.Extensions;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace EntityFrameworkCore.PostgreSQL.SimpleBulks.DirectDelete;
 
@@ -13,7 +16,29 @@ public static class DbContextExtensions
         return dbContext.CreateBulkDeleteBuilder<T>()
              .WithId(table.PrimaryKeys)
              .ToTable(table)
-    .WithBulkOptions(options)
-      .SingleDelete(data);
+             .WithBulkOptions(options)
+             .SingleDelete(data);
+    }
+
+    public static BulkDeleteResult DirectDelete<T>(this DbContext dbContext, T data, Expression<Func<T, object>> keySelector, BulkDeleteOptions options = null)
+    {
+        var table = dbContext.GetTableInfor<T>();
+
+        return dbContext.CreateBulkDeleteBuilder<T>()
+             .WithId(keySelector)
+             .ToTable(table)
+             .WithBulkOptions(options)
+             .SingleDelete(data);
+    }
+
+    public static BulkDeleteResult DirectDelete<T>(this DbContext dbContext, T data, IReadOnlyCollection<string> keys, BulkDeleteOptions options = null)
+    {
+        var table = dbContext.GetTableInfor<T>();
+
+        return dbContext.CreateBulkDeleteBuilder<T>()
+             .WithId(keys)
+             .ToTable(table)
+             .WithBulkOptions(options)
+             .SingleDelete(data);
     }
 }
