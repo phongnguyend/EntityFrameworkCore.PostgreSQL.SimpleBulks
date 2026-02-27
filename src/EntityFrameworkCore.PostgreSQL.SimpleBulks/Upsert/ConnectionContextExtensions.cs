7e30f458ce82413a34dd7587d06280c1f9110376
Phong Nguyen <nguyendoanphong.it@gmail.com>
2026-02-27 07:14:02 +0700

    .Net 8

--- src/EntityFrameworkCore.PostgreSQL.SimpleBulks/Upsert/ConnectionContextExtensions.cs
+++ src/EntityFrameworkCore.PostgreSQL.SimpleBulks/Upsert/ConnectionContextExtensions.cs
@@ -10,23 +10,33 @@ public static class ConnectionContextExtensions
 {
     public static BulkMergeResult Upsert<T>(this ConnectionContext connectionContext, T data, Expression<Func<T, object>> idSelector, Expression<Func<T, object>> updateColumnNamesSelector, Expression<Func<T, object>> insertColumnNamesSelector, BulkMergeOptions options = null)
     {
+        if (options?.ConfigureWhenNotMatchedBySource != null)
+        {
+            throw new ArgumentException($"{nameof(BulkMergeOptions.ConfigureWhenNotMatchedBySource)} is not supported for Upsert operations.", nameof(options));
+        }
+
         return connectionContext.CreateBulkMergeBuilder<T>()
-    .WithId(idSelector)
-   .WithUpdateColumns(updateColumnNamesSelector)
-       .WithInsertColumns(insertColumnNamesSelector)
-    .ToTable(TableMapper.Resolve<T>(options))
-       .WithBulkOptions(options)
-       .SingleMerge(data);
+            .WithId(idSelector)
+            .WithUpdateColumns(updateColumnNamesSelector)
+            .WithInsertColumns(insertColumnNamesSelector)
+            .ToTable(TableMapper.Resolve<T>(options))
+            .WithBulkOptions(options)
+            .SingleMerge(data);
     }
 
     public static BulkMergeResult Upsert<T>(this ConnectionContext connectionContext, T data, IReadOnlyCollection<string> idColumns, IReadOnlyCollection<string> updateColumnNames, IReadOnlyCollection<string> insertColumnNames, BulkMergeOptions options = null)
     {
+        if (options?.ConfigureWhenNotMatchedBySource != null)
+        {
+            throw new ArgumentException($"{nameof(BulkMergeOptions.ConfigureWhenNotMatchedBySource)} is not supported for Upsert operations.", nameof(options));
+        }
+
         return connectionContext.CreateBulkMergeBuilder<T>()
-       .WithId(idColumns)
-         .WithUpdateColumns(updateColumnNames)
-          .WithInsertColumns(insertColumnNames)
-       .ToTable(TableMapper.Resolve<T>(options))
-      .WithBulkOptions(options)
-     .SingleMerge(data);
+            .WithId(idColumns)
+            .WithUpdateColumns(updateColumnNames)
+            .WithInsertColumns(insertColumnNames)
+            .ToTable(TableMapper.Resolve<T>(options))
+            .WithBulkOptions(options)
+            .SingleMerge(data);
     }
 }
